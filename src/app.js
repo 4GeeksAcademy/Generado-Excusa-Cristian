e156f8bcc2f347053de05ac4a10c58a502e8e776
Cristian Trapiello <119610379+CrisMader@users.noreply.github.com>
2025-10-29 11:58:58 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+window.onload = function () {
+  const randomNum = (array) => {
+    let random = Math.floor(Math.random() * array.length)
+    return array[random];
+  }
+
+  const concatenar = () => {
+    let excuse = `${randomNum(who)} ${randomNum(action)} ${randomNum(what)} ${randomNum(when)}`;
+    return excuse;
+  }
+
+  console.log(concatenar())
+
+  document.getElementById(`excuse`).innerHTML = concatenar();
+
+  document.getElementById(`buttonRandom`).addEventListener(`click`, () =>{
+    document.getElementById(`excuse`).innerHTML = concatenar();
+  })
 };
